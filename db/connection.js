1ae92caac29f087267b1a94ac77fdd1b786e97c3
SuperMVS1991 <150755455+SuperMVS1991@users.noreply.github.com>
2024-03-31 12:58:41 -0400

    first commit

---
+++ db/connection.js
@@ -0,0 +1,17 @@
+// Connect to the database
+mongoose.connect('mongodb://localhost/your_database', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+// Get the default connection
+const db = mongoose.connection;
+
+// Handle connection events
+db.on('error', console.error.bind(console, 'Connection error:'));
+db.once('open', () => {
+    console.log('Connected to the database');
+});
+
+// Export the connection
+module.exports = db;
---
+++ models/thought.js
@@ -0,0 +1,58 @@
+const { Schema, Types } = require('mongoose');
+
+const reactionSchema = new Schema({
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new mongoose.Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => new Date(timestamp).toLocaleString(), // Format timestamp on query
+    },
+  });
+  
+
+const thoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    reactions: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Reaction',
+        },
+    ],
+},
+{
+    toJSON: {
+        virtuals: true,
+    },
+    id: false,
+});
+
+reactionsSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+module.exports = { thoughtSchema, reactionSchema };
